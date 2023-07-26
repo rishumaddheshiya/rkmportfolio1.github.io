37e81ccf63d68ef8848dcf79f63664868cc2865a
rishumaddheshiya <rishumaddheshiya999@gmail.com>
2023-07-26 09:04:37 +0530

    rishu portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+<div class="html"><img src="icons8-css-filetype-64.png" alt="">
+  
+  
+   <div class="html"><img src="icons8-javascript-64.png" alt="">
+  
+  
+   <div class="html"><img src="icons8-c-64.png" alt="">
+  
+  
+   <div class="html"><img src="icons8-c-48.png" alt="" width="60px">scroll-behavior: smooth;
+    scroll-padding-top: 2rem;
+    list-style: none;
+    text-decoration: none;
+    box-sizing: border-box;</img>
